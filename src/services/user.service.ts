a94aeb412bab4c39e10f5ce0a4ef4f2cdf254640
Bianca Franco <bianca.franco@dtidigital.com.br>
2023-10-07 18:38:14 -0300

    update user service

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,40 +1,34 @@
-import { Request, Response } from "express";
-import { UsersService } from "./"
-import { removeSecuredFields, generateSecurePassword } from "../utils/users.utils";
 
-export class UsersController {
-  public async listUsers(req: Request, res: Response) {
-    const usersService = UsersService.getInstance();
-    const users = await usersService.list(100, 0);
-    const updatedUsers = users.map(removeSecuredFields);
-    res.status(200).send(updatedUsers);
+export class UsersService{
+  private static instance: UsersService;
+  public static getInstance(): UsersService {
+    if (!UsersService.instance) {
+      UsersService.instance = new UsersService();
+    }
+    return UsersService.instance;
   }
-  public async getUserById(req: Request, res: Response) {
-    const usersService = UsersService.getInstance();
-    const user = await usersService.readById(req.params.userId);
-    res.status(200).send(removeSecuredFields(user));
+
+  public create(resource: any) {
+    return "usuário criado"; 
   }
-  public async createUser(req: Request, res: Response) {
-    const usersService = UsersService.getInstance();
-    const password = Buffer.from(req.body.password);
-    req.body.password = await generateSecurePassword(password);
-    req.body.permissionLevel = CommonPermissionMiddleware.BASIC_PERMISSION;
-    const userId = await usersService.create(req.body);
-    res.status(201).send({ id: userId });
+
+  public deleteById(resourceId: any) {
+    return "usuario deletado";
   }
-  public async patch(req: Request, res: Response) {
-    const usersService = UsersService.getInstance();
-    await usersService.patchById(req.body);
-    res.status(204).send("");
+
+  public list(limit: number, page: number) {
+    return "lista de usuários";
   }
-  public async put(req: Request, res: Response) {
-    const usersService = UsersService.getInstance();
-    await usersService.updateById(req.body);
-    res.status(204).send("");
+
+  public getById(resourceId: any) {
+    return "usuário encontrado por id";
   }
-  public async removeUser(req: Request, res: Response) {
-    const usersService = UsersService.getInstance();
-    await usersService.deleteById(req.params.userId);
-    res.status(204).send("");
+
+  public updateById(resource: any) {
+    return "usuário editado por id";
+  }
+
+  public async getByEmail(email: string) {
+    return "usuário encontrado por id";
   }
 }
