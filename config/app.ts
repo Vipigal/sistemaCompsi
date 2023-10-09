2a1c7698effde955fa7f07f048351be359b8cb68
Vinicius Pinho <viniciuspgalvao@hotmail.com>
2023-10-09 20:41:56 -0300

    instalacao sequelize + auto refs eslint

--- config/app.ts
+++ config/app.ts
@@ -1,16 +1,15 @@
 import express from "express";
-import userRouter from "../src/routes/usersRoute"
-import dotenv from "dotenv"
+import userRouter from "../src/routes/usersRoute";
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
 const app = express();
 
 app.use("/api/users", userRouter);
 
 app.get("/", async (req, res) => {
-	return res.json({ mensagem: "homepage" });
+  return res.json({ mensagem: "homepage" });
 });
-
 
 export default app;
