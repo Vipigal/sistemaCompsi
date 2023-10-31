e5b71df36f70d0f7c510b5967d603d0aa96fca22
Vinicius Pinho <viniciuspgalvao@hotmail.com>
2023-10-31 00:22:38 -0300

    adicionado try/catch nas rotas de post + rota get

--- src/adapters/postRoutes.ts
+++ src/adapters/postRoutes.ts
@@ -1,12 +1,26 @@
 import express, { Request, Response } from "express";
 const router = express.Router();
 import postService from "../domain/postService";
+import { TrataErrorUtil } from "../utils/errorHandler";
 
-router.post("/", async (req: Request, res: Response) => {
-  const post = await postService.createPost(req.body);
-  res.status(200).send(post);
+router.get("/:ID", async (req: Request, res: Response) => {
+  try {
+    const post = postService.getPostById(parseInt(req.params.ID));
+    res.status(200).json(post);
+  } catch (err: unknown) {
+    const error = TrataErrorUtil(err);
+    res.status(error.status).json(error.message);
+  }
 });
 
-
+router.post("/", async (req: Request, res: Response) => {
+  try {
+    await postService.createPost(req.body);
+    res.status(200).json("Post criado com sucesso");
+  } catch (err) {
+    const error = TrataErrorUtil(err);
+    res.status(error.status).json(error.message);
+  }
+});
 
 export default router;
