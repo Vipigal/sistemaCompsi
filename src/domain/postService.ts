2b021d045d527f6f8a0f027739e67ecaecf2c17e
Vinicius Pinho <viniciuspgalvao@hotmail.com>
2023-10-31 00:25:20 -0300

    trocado metodos de usuario para post

--- src/domain/postService.ts
+++ src/domain/postService.ts
@@ -1,7 +1,4 @@
-import { get } from "http";
 import { PostRepository } from "../repositories/postRepository";
-import { TrataErrorUtil } from "../utils/errorHandler";
-import { UserAttributes } from "./userService";
 
 export type UserType = "ALUNO" | "ADMIN" | "GERENCIAL";
 
@@ -13,80 +10,19 @@ export interface PostAttributes {
   userType: UserType;
   authorId: number;
 }
-const userService = {
-  async createUser(body: UserAttributes) {
-    try {
-      const { name, password, email, contactNumber, userType, description } = body;
-      const existingUser = await UserRepository.getUserByEmail(email);
-
-      if (existingUser) 
-        throw new Error("email em uso")  
-
-      if (!["ALUNO", "ADMIN", "GERENCIAL"].includes(userType)) 
-        throw new Error("tipo de usuário invalido")
-    
-        await UserRepository.createUser(body);
-        return "Usuario criado";
-        
-      } catch (error: unknown) {
-       return TrataErrorUtil(error);
+const postService = {
+  createPost: async (body: PostAttributes) => {
+    if (!body.title || !body.content) {
+      throw new Error("O título e o conteúdo são obrigatórios.");
     }
+    await PostRepository.createPost(body);
+    return "Post criado";
   },
-
-  async deleteUserById(id: number) {
-    try {
-    const existingUser = await UserRepository.getUserById(id);
-      if (!existingUser) 
-        throw new Error("Usuario não existe");
-      
-      UserRepository.deleteUserById(id);
-      return "Usuário deletado";
-    } catch (error: unknown) {
-        return TrataErrorUtil(error);
-  }
-},
-
-  async listUsers(limit: number, page: number) {
-    try {
-      if (page < 1) {
-        throw new Error("O número da página deve ser maior ou igual a 1.");
-      }
-
-      const users = await UserRepository.getUsers(
-        // limit,
-        // offset: (page - 1) * limit, // Calcula o offset a partir da página
-      );
-      return users;
-    } catch (error: unknown) {
-        return TrataErrorUtil(error);
-  }
-},
-
-  getUserById(id: number) {
-    try {
-      const user = UserRepository.getUserById(id);
-      return user
-    } catch (error: unknown) {
-        return TrataErrorUtil(error);
-  }
-},
-
-  updateUserById(id: number, body: Partial<UserAttributes>) {
-    try {
-      const updatedUser = UserRepository.updateUserById(id, body);
-      return "Usuário editado por id";
-    } catch (error: unknown) {
-        return TrataErrorUtil(error);
-  }},
-
-  async getUserByEmail(email: string) {
-    try {
-      const user = UserRepository.getUserByEmail(email);
-      return user;
-    } catch (error: unknown) {
-        return TrataErrorUtil(error);
-    }
+  getPostById: async (id: number) => {
+    const post = await PostRepository.getPostById(id);
+    if (PostRepository) return post;
+    else throw new Error("Nenhum post cadastrado");
   },
 };
 
-export default userService;
+export default postService;
