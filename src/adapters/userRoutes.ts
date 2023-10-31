e8129cd687c185a416b8eb55e608582c201946c2
Vinicius Pinho <viniciuspgalvao@hotmail.com>
2023-10-31 00:24:59 -0300

    adicionados try/catch nas rotas de usuarios

--- src/adapters/userRoutes.ts
+++ src/adapters/userRoutes.ts
@@ -1,4 +1,4 @@
-import express, { NextFunction, Request, Response } from "express";
+import express, { Request, Response } from "express";
 const router = express.Router();
 import userService from "../domain/userService";
 import {
@@ -8,6 +8,7 @@ import {
   login,
   logout,
 } from "../middlewares/auth";
+import { TrataErrorUtil } from "../utils/errorHandler";
 
 router.post("/login", checkIfLoggedIn, login, (req: Request, res: Response) => {
   res.json("Login bem sucedido!");
@@ -17,42 +18,61 @@ router.post("/logout", logout, (req: Request, res: Response) => {
   res.json("Logout bem sucedido!");
 });
 
-router.post("/", async (req: Request, res: Response) => {
-  const user = await userService.createUser(req.body);
-  res.status(200).send(user);
-});
-
 //Retorna todos os usuarios cadastrados no sistema.
 router.get(
   "/",
   auth,
   checkRole(["ADMIN", "GERENCIAL"]),
-  async (req: Request, res: Response, next: NextFunction) => {
+  async (req: Request, res: Response) => {
     try {
       const users = await userService.listUsers(100, 1);
-      res.status(200).send(users);
-    } catch (error) {
-      next(error);
+      res.status(200).json(users);
+    } catch (err: unknown) {
+      const error = TrataErrorUtil(err);
+      res.status(error.status).json(error.message);
     }
   }
 );
 
 router.get("/:ID", async (req: Request, res: Response) => {
-  const user = userService.getUserById(parseInt(req.params.ID));
-  res.status(200).send(user);
+  try {
+    const user = userService.getUserById(parseInt(req.params.ID));
+    res.status(200).json(user);
+  } catch (err: unknown) {
+    const error = TrataErrorUtil(err);
+    res.status(error.status).json(error.message);
+  }
 });
 
-router.delete("/:ID", async (req: Request, res: Response) => {
-  const user = await userService.deleteUserById(parseInt(req.params.ID));
-  res.status(200).send(user);
+router.post("/", async (req: Request, res: Response) => {
+  try {
+    await userService.createUser(req.body);
+    res.status(200).json("Usuário criado com sucesso!");
+  } catch (err: unknown) {
+    const error = TrataErrorUtil(err);
+    res.status(error.status).json(error.message);
+  }
 });
 
 router.put("/:ID", async (req: Request, res: Response) => {
-  const user = await userService.updateUserById(
-    parseInt(req.params.ID),
-    req.body
-  );
-  res.status(200).send(user);
+  try {
+    const user = userService.updateUserById(parseInt(req.params.ID), req.body);
+    res.status(200).json(user);
+  } catch (err: unknown) {
+    const error = TrataErrorUtil(err);
+    res.status(error.status).json(error.message);
+  }
+});
+router.delete("/:ID", async (req: Request, res: Response) => {
+  try {
+    await userService.deleteUserById(parseInt(req.params.ID));
+    res
+      .status(200)
+      .json(`Usuário com id ${req.params.ID} deletado com sucesso!`);
+  } catch (err: unknown) {
+    const error = TrataErrorUtil(err);
+    res.status(error.status).json(error.message);
+  }
 });
 
 export default router;
