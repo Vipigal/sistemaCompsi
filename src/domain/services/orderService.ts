3b14fffbc59ff73ee6fad8790dfa4fd64bc383ca
Bianca Franco <biancagabr@gmail.com>
2023-11-05 22:56:18 -0300

    list by product

--- src/domain/services/orderService.ts
+++ src/domain/services/orderService.ts
@@ -17,14 +17,36 @@ const orderService = {
     await OrderRepository.createOrder(body, email);
     return "Pedido criado com sucesso";
   },
+
   listOrders: async (limit: number, page: number) => {
     const orders = await OrderRepository.listOrders();
     if (orders) return orders;
     else throw new Error("Não há pedidos cadastrados");
   },
   
+  async updateOrderbyId(id: number, body: Partial<OrderAttributes>) {
+    const product = await OrderRepository.updateOrderByID(id, body);
+    if (product) return "Produto editado por ID";
+    else throw new Error("Erro ao editar produto");
+  },
 
+  async listOrdersByProduct(productName: string): Promise<OrderAttributes[] | null>{
+    if(!await productService.getProductByName(productName))
+    throw new Error("Produto Não encontrado")
+    const orders = await OrderRepository.listOrdersByProduct(productName); 
+    return orders; 
+  },
 
 };
 
+
+
 export default orderService;
+
+/*
+
+listOrdersByUser(email: string): Promise<OrderAttributes[] | null>;
+getOrderByID(id: number): Promise<OrderAttributes | null>;
+deleteOrderByID(id: number): void;
+
+*/
